767a143f5f327b535cf2e34915ffde2440d968ef
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-07-21 14:39:42 -0500

    Added order_by_exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,79 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '%-10-31'
+AND birth_date LIKE '%-10-%';
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
