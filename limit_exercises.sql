296d69bfc17e9f97519031f169d710da5da15525
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-07-21 16:06:03 -0500

    added limit_exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+
+DESCRIBE employees;
+
+-- List the first 10 distinct last name sorted in descending order
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- Update query for employees born on christmas and hired in the 90s to limit to the first five
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+-- Update it to show the 10th page of last query
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
